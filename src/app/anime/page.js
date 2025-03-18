ce067e10ec1faf94e8ff34a72ef3781def3e51a3
ket8n <ketanchopade.kc@gmail.com>
2025-03-18 23:30:27 +0530

    Refactor: remove unwanted comments

--- src/app/anime/page.js
+++ src/app/anime/page.js
@@ -1,13 +1,10 @@
 import AnimeCard from "../components/AnimeCard";
 
 export default async function Page() {
-  // const [animeList, setAnimeList] = useState([]);
-
-  // useEffect(() => {}, []);
   const data = await fetch("https://api.jikan.moe/v4/top/anime");
   const dataJSON = await data.json();
   const animeList = dataJSON.data || [];
-  console.log(animeList);
+
   return (
     <div className="flex flex-wrap gap-4 justify-center items-start">
       {animeList.map((anime) => (
