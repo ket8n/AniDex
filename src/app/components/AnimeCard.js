dedfc27529ef00a6c9fc91b3220f6e9a5c06d9cf
ket8n <ketanchopade.kc@gmail.com>
2025-03-18 23:31:45 +0530

    Add: navigation from anime card to anime details pagre

--- src/app/components/AnimeCard.js
+++ src/app/components/AnimeCard.js
@@ -1,20 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function AnimeCard({ animeDetails: anime }) {
   return (
-    <div className="border-2 w-60 min-h-[29rem] p-3 m-3 flex flex-col">
-      <h3>{anime.titles[0].title}</h3>
-      <Image
-        src={anime.images.webp.image_url}
-        width="0"
-        height="0"
-        sizes="100vw"
-        className="w-full h-auto"
-        alt={`Anime - ${anime.titles[0].title}`}
-      />
-      <p>Rating: {anime.rating}</p>
-      <p>Score: {anime.score}</p>
-      <p>Rank: {anime.rank}</p>
-    </div>
+    <Link href={`/anime/${anime.mal_id}`}>
+      <div className="border-2 w-60 min-h-[29rem] p-3 m-3 cursor-pointer">
+        <h3 className="overflow-hidden whitespace-nowrap text-ellipsis">
+          {anime.titles[0].title}
+        </h3>
+        <Image
+          src={anime.images.webp.image_url}
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full h-auto"
+          alt={`Anime - ${anime.titles[0].title}`}
+        />
+        <p>Rating: {anime.rating}</p>
+        <p>Score: {anime.score}</p>
+        <p>Rank: {anime.rank}</p>
+      </div>
+    </Link>
   );
 }
