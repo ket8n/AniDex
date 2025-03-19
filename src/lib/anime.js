c2a16a1ea6d2c21ebd99ae86427ce318c877ee06
ket8n <ketanchopade.kc@gmail.com>
2025-03-19 22:29:11 +0530

    Refactor: use constant url from constants.js file

--- src/lib/anime.js
+++ src/lib/anime.js
@@ -1,18 +1,20 @@
+import { BASE_EXTERNAL_API_URL } from "@/utils/constants";
+
 export async function getTopAnime() {
-  const data = await fetch("https://api.jikan.moe/v4/top/anime");
+  const data = await fetch(`${BASE_EXTERNAL_API_URL}/top/anime`);
   const dataJSON = await data.json();
   return dataJSON.data || [];
 }
 
 export async function getAnimeById(id) {
-  const data = await fetch(`https://api.jikan.moe/v4/anime/${id}`);
+  const data = await fetch(`${BASE_EXTERNAL_API_URL}/anime/${id}`);
   const dataJSON = await data.json();
   return dataJSON.data || [];
 }
 
 export async function getAnimeCharactersById(id) {
   const data_characters = await fetch(
-    `https://api.jikan.moe/v4/anime/${id}/characters`
+    `${BASE_EXTERNAL_API_URL}/anime/${id}/characters`
   );
   const data_characters_JSON = await data_characters.json();
   return data_characters_JSON.data || [];
